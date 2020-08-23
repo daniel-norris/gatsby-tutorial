b73df4ecbd6be1390aef27fa056e11f0bcb16c40
Daniel Norris <dan.norris@hotmail.com>
2020-08-23 10:42:42 +0100

    created a site config file

---
+++ src/config/index.js
@@ -0,0 +1,50 @@
+module.exports = {
+    author: 'Dan Norris',
+    siteTitle: 'Dan Norris - Portfolio',
+    siteShortTitle: 'DN',
+    siteDescription:
+        'v2 personal portfolio. Dan is a Software Engineer and based in Bristol, UK',
+    siteLanguage: 'en_UK',
+    socialMedia: [
+        {
+            name: 'Twitter',
+            url: 'https://twitter.com/danielpnorris',
+        },
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/danielpnorris/',
+        },
+        {
+            name: 'Medium',
+            url: 'https://medium.com/@dan.norris',
+        },
+        {
+            name: 'GitHub',
+            url: 'https://github.com/daniel-norris',
+        },
+        {
+            name: 'Dev',
+            url: 'https://dev.to/danielnorris',
+        },
+    ],
+    navLinks: {
+        menu: [
+            {
+                name: 'About',
+                url: '/#about',
+            },
+            {
+                name: 'Work',
+                url: '/#work',
+            },
+            {
+                name: 'Contact',
+                url: '/#contact',
+            },
+        ],
+        button: {
+            name: 'Get In Touch',
+            url: '/#contact',
+        },
+    },
+}
