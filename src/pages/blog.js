fae83c24c75e0d865e38b5622f400b97c9ed9acb
Daniel Norris <dan.norris@hotmail.com>
2020-08-31 11:38:14 +0100

    created a blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default ({ data }) => {
+    return (
+        <Layout>
+            <h1>Blog</h1>
+            <p>Our blog articles will go here!</p>
+        </Layout>
+    )
+}
