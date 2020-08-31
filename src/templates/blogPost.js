e09eb4c3e61f711b2125afdbd93f5c82ab8e64c3
Daniel Norris <dan.norris@hotmail.com>
2020-08-31 19:04:14 +0100

    created blog post template

---
+++ src/templates/blogPost.js
@@ -0,0 +1,42 @@
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default ({ data }) => {
+    const { frontmatter, body } = data.mdx
+
+    return (
+        <Layout>
+            <section
+                className="w-2/4 my-8 mx-auto container"
+                style={{ minHeight: '80vh' }}
+            >
+                <h1 className="text-3xl sm:text-5xl font-bold">
+                    {frontmatter.title}
+                </h1>
+                <div className="flex justify-between">
+                    <p className="text-base text-gray-600">
+                        {frontmatter.date}
+                    </p>
+                </div>
+
+                <div className="mt-8 text-base font-light">
+                    <MDXRenderer>{body}</MDXRenderer>
+                </div>
+            </section>
+        </Layout>
+    )
+}
+
+export const pageQuery = graphql`
+    query BlogPostQuery($id: String) {
+        mdx(id: { eq: $id }) {
+            id
+            body
+            frontmatter {
+                title
+                date(formatString: "Do MMM YYYY")
+            }
+        }
+    }
+`
